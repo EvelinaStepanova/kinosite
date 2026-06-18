d9c97a55b1bc7e1becb9eb8f9037b0fbb5bbd13b
EvelinaStepanova <104110209+EvelinaStepanova@users.noreply.github.com>
2026-06-18 20:52:10 +0800

    Add files via upload

---
+++ js/theme.js
@@ -0,0 +1,20 @@
+const themeButton = document.querySelector(".header_theme");
+
+const THEME_KEY = "kinosite_theme";
+
+const savedTheme = localStorage.getItem(THEME_KEY);
+
+if(savedTheme === "dark"){
+    document.body.classList.add("dark-theme");
+}
+if(themeButton){
+    themeButton.addEventListener("click", ()=>{
+        document.body.classList.toggle("dark-theme");
+        const isDarkTheme = document.body.classList.contains("dark-theme");
+        if(isDarkTheme){
+            localStorage.setItem(THEME_KEY, "dark");
+        }else{
+            localStorage.setItem(THEME_KEY, "light");
+        }
+    });
+}
